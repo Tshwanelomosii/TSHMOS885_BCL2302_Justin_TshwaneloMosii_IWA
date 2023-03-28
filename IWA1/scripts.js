280bfaf09a8d2d007466e96ec86f902c41cf2a3e
Tshwanelomosii <percymosii1@gmail.com>
2023-03-28 15:16:42 +0200

    fixed log and added semi-colon

--- IWA1/scripts.js
+++ IWA1/scripts.js
@@ -1,29 +1,32 @@
-const MAX_NUMBER =10
-const MIN_NUMBER=4
-const number = document.querySelector('[data-key=number]')
-const add =document.querySelector('[data-key=subtract]')
-const subtract =document.querySelector('[data-key=add]')
+const MAX_NUMBER = 10
+const MIN_NUMBER = -4
 
-const subtracthandler = () =>{
+const number = document.querySelector('[data-key="number"]')
+const subtract = document.querySelector('[data-key="subtract"]')
+const add = document.querySelector('[data-key="add"]')
+
+const subtracthandler = () => {
     const newValue = parseInt(number.value) -1
-    number.value= newValue
-    if(number.value<= MIN_NUMBER){ 
-        subtract.disabled=true
-    } else{
-        subtract.disabled=false
-    }
-}
-const addhandler = () =>{
-    const newValue = parseInt(number.value) +1
-    number.value= newValue
+    number.value = newvalue
 
-    if(number.value>= MAX_NUMBER){ 
-        add.disabled=true
+    if(number.value <= MIN_NUMBER) { 
+        subtract.disabled = true
     } else{
-        add.disabled=false
+        subtract.disabled = false
     }
 }
-subtract.addEventListener('click',subtracthandler);
-add.addEventListener('click',addhandler);
+
+const addhandler = () => {
+    const newValue = parseInt(number.value) + 1
+    number.value = newvalue
+
+    if(number.value >= MAX_NUMBER){ 
+        add.disabled = true
+    } else{
+        add.disabled = false
+    }
+}
+subtract.addEventListener('click', subtracthandler);
+add.addEventListener('click', addhandler);
 
 
