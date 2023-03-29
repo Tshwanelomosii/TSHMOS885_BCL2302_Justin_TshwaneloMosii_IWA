86f9a894904e76930b8e93d2736f6a5c0c094224
Tshwanelomosii <percymosii1@gmail.com>
2023-03-29 16:34:50 +0200

    inserted export tags and import tags

---
+++ IWA3/challenge2/nwabisa.js
@@ -0,0 +1,6 @@
+const surname = "Gabe"
+const firstname = "Nwabisa"
+const role = "CEO"
+
+const display= firstname + " " + surname  
+document.querySelector('#nwabisa').innerText = display
---
+++ IWA3/challenge2/scripts.js
@@ -0,0 +1,6 @@
+ import"./alex.js"
+ import"./johannes.js"
+ import"./nwabisa.js"
+
+
+console.log('Roles:', nwabisa.role, johanes.role, alex.role)
