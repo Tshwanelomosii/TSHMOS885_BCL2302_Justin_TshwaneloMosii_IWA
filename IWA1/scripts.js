62166d3c56cda6aacfed3b6dbf9f335986fb938a
Tshwanelomosii <percymosii1@gmail.com>
2023-03-29 10:40:37 +0200

    link javascript

--- IWA1/scripts.js
+++ IWA1/scripts.js
@@ -1,32 +1,38 @@
-const MAX_NUMBER = 10
-const MIN_NUMBER = -4
+const MAX_NUMBER = 100
+const MIN_NUMBER = -20
+const STEP_AMOUNT = 5
 
 const number = document.querySelector('[data-key="number"]')
 const subtract = document.querySelector('[data-key="subtract"]')
 const add = document.querySelector('[data-key="add"]')
 
 const subtracthandler = () => {
-    const newValue = parseInt(number.value) -1
-    number.value = newvalue
+    const newValue = parseInt(number.value) - STEP_AMOUNT;
+    number.value = newValue
 
-    if(number.value <= MIN_NUMBER) { 
-        subtract.disabled = true
-    } else{
-        subtract.disabled = false
+    if(add.disabled === true){
+        add.disabled = false
     }
+
+    if(newValue <= MIN_NUMBER) { 
+        subtract.disabled = true
+    } 
 }
 
 const addhandler = () => {
-    const newValue = parseInt(number.value) + 1
-    number.value = newvalue
+    const newValue = parseInt(number.value) + STEP_AMOUNT;
+    number.value = newValue
+
+    if(subtract.disabled === true){
+        subtract.disabled = false
+    }
 
     if(number.value >= MAX_NUMBER){ 
         add.disabled = true
-    } else{
-        add.disabled = false
-    }
+    } 
 }
-subtract.addEventListener('click', subtracthandler);
-add.addEventListener('click', addhandler);
+
+subtract.addEventListener('click', subtracthandler)
+add.addEventListener('click', addhandler)
 
 
