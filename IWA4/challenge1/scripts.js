0993527303a434a8165acb4745e3ad4e75dc29dc
Tshwanelomosii <percymosii1@gmail.com>
2023-03-30 14:51:26 +0200

    fixed error giving variable name

--- IWA4/challenge1/scripts.js
+++ IWA4/challenge1/scripts.js
@@ -1,16 +1,17 @@
-const date = 2050
-const status = 'parent'
+let year = 2050
+let status = 'parent'
 let count = 0
 
-if ( date) {
+if ( year) {
 	console.log("January", 'New Year’s Day')
-	console.log("March", 'Human Rights Day')
+	console.log('March', 'Human Rights Day')
 	
-	console.log('April', 'Family Day')
-	console.log('April', 'Freedom Day')
-	 count = count + 4
+    let date = 'April'
+	console.log(date, 'Family Day')
+	console.log(date, 'Freedom Day')
+	count = count + 4
 
-	if (status = "student") {
+	if (status == "student") {
 	  console.log('June', 'Youth Day')
 		 count = count + 1
   }
@@ -18,19 +19,20 @@ if ( date) {
 	console.log('August', 'Women’s Day')
 	console.log('September', 'Heritage Day')
 	 
-	console.log('December', 'Day of Reconciliation')
+    date = 'December'
+	console.log(date, 'Day of Reconciliation')
 	 count = count + 3
 
-	if (status = "parent") {
-	  console.log('December', 'Christmas Day')
+	if (status == "parent") {
+	  console.log(date, 'Christmas Day')
 		 count = count + 1
   }
 
-	console.log('December', 'Day of Goodwill')
-	let count = count + 1
+	console.log(date, 'Day of Goodwill')
+	 count = count + 1
 }
 
 console.log('Your status is:', status)
-console.log('The year is:', date)
+console.log('The year is:', year)
 console.log('The total holidays is:', count)
 
