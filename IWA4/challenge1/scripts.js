02f31e7eaf020a03d47f22a02cbef5bd1ffeb173
Tshwanelomosii <percymosii1@gmail.com>
2023-03-30 14:16:48 +0200

    changed student as first declaration

--- IWA4/challenge1/scripts.js
+++ IWA4/challenge1/scripts.js
@@ -1,35 +1,36 @@
 const date = 2050
-const status = 'student'
-const count = 0
+const status = 'parent'
+let count = 0
 
-if (date = 2050) {
+if ( date) {
 	console.log("January", 'New Year’s Day')
 	console.log("March", 'Human Rights Day')
-	date = 'April'
-	console.log(date, 'Family Day')
-	console.log(date, 'Freedom Day')
-	let count = count + 4
+	
+	console.log('April', 'Family Day')
+	console.log('April', 'Freedom Day')
+	 count = count + 4
 
 	if (status = "student") {
 	  console.log('June', 'Youth Day')
-		let count = count + 1
+		 count = count + 1
   }
 
 	console.log('August', 'Women’s Day')
 	console.log('September', 'Heritage Day')
-	date = 'December'
-	console.log(date, 'Day of Reconciliation')
-	let count = count + 3
+	 
+	console.log('December', 'Day of Reconciliation')
+	 count = count + 3
 
 	if (status = "parent") {
-	  console.log(date, 'Christmas Day')
-		let count = count + 1
+	  console.log('December', 'Christmas Day')
+		 count = count + 1
   }
 
-	console.log(date, 'Day of Goodwill')
+	console.log('December', 'Day of Goodwill')
 	let count = count + 1
 }
 
 console.log('Your status is:', status)
 console.log('The year is:', date)
 console.log('The total holidays is:', count)
+
