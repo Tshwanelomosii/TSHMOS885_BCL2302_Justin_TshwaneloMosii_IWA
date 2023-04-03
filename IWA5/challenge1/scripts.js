e7a96981a3c7f7bfe176e06f015b009bc23c0006
Tshwanelomosii <percymosii1@gmail.com>
2023-04-03 14:27:17 +0200

    fixed line 9,13 added currency value and equal sign

--- IWA5/challenge1/scripts.js
+++ IWA5/challenge1/scripts.js
@@ -2,28 +2,34 @@ FREE_WARNING = 'Free shipping only applies to single customer orders'
 BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
 NONE_SELECTED = '0'
 
-if (location = RSA) { shipping === 400 && currency === 'R' }
+if (location = RSA) {
+	shipping === 400 && currency === 'R' 	
 
-if location = NAM
-shipping = 600 
-else shipping = 800
+if (location = NAM) {
+	shipping = 600 && currency === '$' //added currency value and equal sign 
+}
+
+else {
+	shipping = 800 && currency === '$' //added currency value and equal sign 
+}
+}
 
 shoes = 300 * 1
-toys - 100 * 5
+toys = 100 * 5  //added equal sign
 shirts = 150 * 'NONE_SELECTED'
-batteries 35 * 2
+batteries = 35 * 2  //added equal sign
 pens = 5 * 'NONE_SELECTED' 
 
 shipping = null
 currency = $
 
-if (shoes + batteries + pens + shirts > 1000 &&  ) {
+if (shoes + batteries + pens + shirts >= 1000 && currency === 'R' || 60 && currency === '$' ) {
 	if (location = NAM && customers < 2) {
 			if (location = RSA)
 		    shipping = 0 || calcShipping
 		}
-	}
 }
+
 
 if (shipping = 0) && (customers !=== 1) { console.log(WARNING) }
 
