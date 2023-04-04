3c2e18c3b82dda1c80a5032dd2c3ffd1e260d118
Tshwanelomosii <percymosii1@gmail.com>
2023-04-04 11:45:28 +0200

    fixed typo by adding zero instead of letter O

--- IWA5/challenge1/scripts.js
+++ IWA5/challenge1/scripts.js
@@ -2,17 +2,21 @@ const FREE_WARNING = 'Free shipping only applies to single customer orders' // a
 const BANNED_WARNING = 'Unfortunately we do not ship to your country of residence' //added G in warning typo error and added const  to  define variables 
 const 	NONE_SELECTED = '0' // added const to  define variables 
 let location = 'RSA'
-let shipping = 400 ||  600 || 800 || null
+let shipping = 0 
 let currency = '$' || 'R'
+const customers = '1'
+let  calcShipping = null 
+
+
 
 
 if (location) {
 	shipping === 400 && currency === 'R' 
 	
 
-location = 'NAM'
+location = 'NAM' //declared location as NUM
 if (location) {
-	shipping = 600 && currency === '$' //added currency value and equal sign 
+	 shipping = 600 && currency === '$' //added currency value and equal sign 
 }
 
 else {
@@ -22,24 +26,34 @@ else {
 
 let shoes = 300 * 1
 let toys = 100 * 5  //added equal sign
-let shirts = 150 * 'NONE_SELECTED'
+let shirts = 150 * NONE_SELECTED
 let batteries = 35 * 2  //added equal sign
-let pens = 5 * 'NONE_SELECTED' 
+let pens = 5 * NONE_SELECTED 
 
 
 if (shoes + batteries + pens + shirts >= 1000 && currency === 'R' || 60 && currency === '$' ) {
-	if (location = NAM && customers < 2) {
-			if (location = RSA)
-		    shipping = 0 || calcShipping
+	location = 'NAM'
+	if (location && customers < 2) {
+
+		location = 'RSA' // declared RSA 
+		if (location)
+		shipping = 0
 		}
 }
 
 
 if (shipping == 0 && customers !== 1) {
-	console.log('FREE_WARNING') }
+	console.log(FREE_WARNING)
 
-location = 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
+	location = 'NK' //declared NK
+	if(location){
+		currency = null
+		console.log(BANNED_WARNING)
+	}
 
-customers = '1'
+}
 
-currency = null
+
+    console.log('price', currency, shoes + batteries + pens + shirts + shipping)
+
+
---
+++ IWA6/challenge1/scripts.js
@@ -0,0 +1,10 @@
+const primaryPhone = '0748105141' // fixed typo by adding zero instead of letter O
+const secondaryPhone = '0219131568'
+
+// Only change below this line
+
+const primaryValid = typeof number == primaryPhone 
+const secondaryValid = typeof number == secondaryPhone
+
+console.log('Primary phone is valid numerical string:', primaryValid)
+console.log('Secondary phone is valid numerical string:', secondaryValid )
