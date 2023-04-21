7dff87878ebf09335ea4c591d5a6714d640529a9
Tshwanelomosii <percymosii1@gmail.com>
2023-04-21 09:28:36 +0200

    debugged iwa 15

--- IWA14/challenge2/scripts.js
+++ IWA14/challenge2/scripts.js
@@ -1,38 +1,53 @@
-// // script.js
+function add(a, b) {   //This code defines three functions: add, multiply, and internal.
+    return a + b;       // The add function takes two parameters, a and b, and returns their sum using the + operator.
+  }
+  
+  function multiply(a, b) {      // The multiply function also takes two parameters, a and b, and returns their product using the * operator.
+    return a * b;
+  }
+  
+  function internal() {  // The internal function does some calculations using the add and multiply functions.  
+    const added = this.add(this.internal.a, this.internal.b);  //It first calculates the sum of the a and b properties of the internal object using the add function, 
+    const multiplied = this.multiply(added, this.internal.c);  //and stores the result in the added variable. It then calculates the product of added and the c property 
+    return multiplied;    //of the internal object using the multiply function, and stores the result in the multiplied variable.Finally, it returns the value of multiplied.
+  }
+  
+  // Not allowed to change below this
+  
+  const example1 = {
+    internal: {
+      a: 2,
+      b: 4,
+      c: 8,
+    },
+    add,
+    multiply,
+    calculate: internal,
+  };
+  
+  const example2 = {
+    internal: {
+      a: 2,
+      b: 2,
+      c: 3,
+    },
+    add,
+    multiply,
+    calculate: internal,
+  };
+  
+  console.log(example1.calculate()); // should log 48
+  console.log(example2.calculate()); // should log 12
+  
 
-// function add = a, b => { a + b }
+  
 
-// function multiply = a, b => { a - b }
 
-// function internal() {
-// 	const added = this.add(this.a, this.b)
-// 	this.multiply(this.a, this.b)
-// 	return this
-// }
 
-// // Not allowed to change below this
 
-// const example1 = {
-// 	internal: {
-// 		a: 2,
-// 		b: 4,
-// 		c: 8,
-// 	},
-// 	add,
-// 	multiply,
-//   calculate: internal
-// }
 
-// const example2 = {
-// 	internal: {
-// 		a: 2,
-// 		b: 2,
-// 		c: 3,
-// 	},
-// 	add,
-// 	multiply,
-//   calculate: internal
-// }
 
-// example1.calculate()
-// example2.calculate()
+
+// Two objects are then created, example1 and example2, each with an internal property containing three properties a, b, and c. They also have add, multiply, and calculate properties. The calculate property is assigned the internal function, which will be called when the calculate property is invoked.
+
+// The last two lines of code call the calculate method on example1 and example2 and log the results to the console. The calculate method will execute the internal function in the context of the object on which it was called, using the this keyword to refer to that object. The expected output is 48 for example1 and 12 for example2, which are the results of the calculations performed by the internal function on each object's properties.
